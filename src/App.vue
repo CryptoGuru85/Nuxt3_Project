38251328a5a7a8dc7a4fef7fb882959e0ab9ddad
storyofsoft <storyofsoft1024@gmail.com>
2022-02-17 18:10:41 +0900

    simple_piano

---
+++ src/App.vue
@@ -0,0 +1,191 @@
+<template>
+  <Piano :notes="notes" :createAnimation="createAnimation" :emitSound="emitSound" />
+  <Midi
+    :notes="notes"
+    :createAnimation="createAnimation"
+    :emitSound="emitSound"
+    :midiTable="midiTable"
+  />
+</template>
+
+<script>
+import Piano from './components/Piano.vue'
+import Midi from './components/Midi.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Piano,
+    Midi
+  },
+  data() {
+    return {
+      emitSound(audio) { // emit and slowly fades the sound away
+        const clone = audio.cloneNode();
+        clone.play();
+        setTimeout(() => (clone.volume = 0.8), 400);
+        setTimeout(() => (clone.volume = 0.6), 800);
+        setTimeout(() => (clone.volume = 0.4), 1200);
+        setTimeout(() => (clone.volume = 0.2), 1600);
+        setTimeout(() => (clone.volume = 0), 2000);
+      },
+      createAnimation(notes, i, emitSound) {
+        var currentKey = document.querySelector("#" + notes[i].note);
+        emitSound(notes[i].obj);
+        currentKey.classList.add("active"); // add a css class to create the illusion of playing through different animations 
+        setTimeout(() => currentKey.classList.remove("active"), 200); // removing the class when the sound is emitted
+      },
+      notes: [{
+        note: "C4",
+        key: "Q",
+        type: "white",
+        obj: new Audio("assets/sounds/C4.mp3")
+      },
+      {
+        note: "Db4",
+        key: "W",
+        type: "black",
+        obj: new Audio("assets/sounds/Db4.mp3")
+      },
+      {
+        note: "D4",
+        key: "A",
+        type: "white close",
+        obj: new Audio("assets/sounds/D4.mp3")
+      },
+      {
+        note: "Eb4",
+        key: "E",
+        type: "black",
+        obj: new Audio("assets/sounds/Eb4.mp3")
+      },
+      {
+        note: "E4",
+        key: "S",
+        type: "white close",
+        obj: new Audio("assets/sounds/E4.mp3")
+      },
+      {
+        note: "F4",
+        key: "D",
+        type: "white",
+        obj: new Audio("assets/sounds/F4.mp3")
+      },
+      {
+        note: "Gb4",
+        key: "T",
+        type: "black",
+        obj: new Audio("assets/sounds/Gb4.mp3")
+      },
+      {
+        note: "G4",
+        key: "F",
+        type: "white close",
+        obj: new Audio("assets/sounds/G4.mp3")
+      },
+      {
+        note: "Ab4",
+        key: "Y",
+        type: "black",
+        obj: new Audio("assets/sounds/Ab4.mp3")
+      },
+      {
+        note: "A4",
+        key: "G",
+        type: "white close",
+        obj: new Audio("assets/sounds/A4.mp3")
+      },
+      {
+        note: "Bb4",
+        key: "U",
+        type: "black",
+        obj: new Audio("assets/sounds/Bb4.mp3")
+      },
+      {
+        note: "B4",
+        key: "H",
+        type: "white close",
+        obj: new Audio("assets/sounds/B4.mp3")
+      },
+      {
+        note: "C5",
+        key: "J",
+        type: "white",
+        obj: new Audio("assets/sounds/C5.mp3")
+      },
+      {
+        note: "Db5",
+        key: "O",
+        type: "black",
+        obj: new Audio("assets/sounds/Db5.mp3")
+      },
+      {
+        note: "D5",
+        key: "K",
+        type: "white close",
+        obj: new Audio("assets/sounds/D5.mp3")
+      },
+      {
+        note: "Eb5",
+        key: "P",
+        type: "black",
+        obj: new Audio("assets/sounds/Eb5.mp3")
+      },
+      {
+        note: "E5",
+        key: "L",
+        type: "white close",
+        obj: new Audio("assets/sounds/E5.mp3")
+      }
+      ],
+      midiTable: [
+        ["", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""],
+        ["", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""],
+        ["", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""],
+        ["", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""],
+        ["", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""],
+        ["", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""],
+        ["", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""],
+        ["", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""],
+        ["", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""],
+        ["", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""],
+        ["", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""],
+        ["", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""],
+        ["", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""],
+        ["", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""],
+        ["", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""],
+        ["", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""],
+        ["", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""],
+        ["", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""],
+        ["", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""],
+        ["", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""],
+        ["", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""],
+        ["", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""],
+        ["", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""],
+        ["", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""]
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="sass">
+@import url('https://fonts.googleapis.com/css2?family=Encode+Sans+SC:wght@400;800&display=swap')
+
+html
+    height: 100%
+
+body
+    margin: 0
+    padding: 0
+    background: rgba(0,0,0,0.75)
+
+@media screen and (max-width: 300px)
+    #app
+        display: none
+
+    #smallestDisplay
+        display: block!important
+        color: white
+        text-align: center
+</style>
---
+++ src/components/Piano.vue
@@ -0,0 +1,121 @@
+<template>
+  <div id="keyboard">
+    <div id="keys">
+      <div v-for="note in notes" :key="note" :class="note.type" :id="note.note" class="key">
+        <!-- keyboard creation loop -->
+        {{ note.key }}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Piano",
+  props: {
+    notes: {
+      type: Array,
+    },
+    emitSound: {
+      type: Function,
+    },
+    createAnimation: {
+      type: Function,
+    },
+  },
+  mounted() {
+    var notes = this.notes;
+    var emitSound = this.emitSound;
+    var createAnimation = this.createAnimation;
+    var player = [];
+
+    for (let i = 0; i < notes.length; i++) {
+      player[i] = () => {
+        // array of arrow functions where each index plays a note
+        createAnimation(notes, i, emitSound);
+      };
+
+      document.querySelector("#" + notes[i].note).addEventListener("click", player[i]); // emit a note when the relative key is clicked
+      window.addEventListener("keydown", function (event) {
+        if (event.key.toLowerCase() == notes[i].key.toLowerCase() && !event.repeat)
+          return player[i](); // emit a note when the relative key on physical keyboard is pressed
+      });
+    }
+  },
+};
+</script>
+
+<style scoped lang="sass">
+@use 'sass:math'
+// credits to Enteleform for the calculations!
+
+$keyboard_Width: 40vw
+$keyboard_Height: ($keyboard_Width * 0.3 )
+$border_Width: ($keyboard_Width * 0.004)
+$whiteKey_Count: 10
+$whiteKey_Height: $keyboard_Height
+$whiteKey_Width: math.div($keyboard_Width, $whiteKey_Count)
+$blackKey_Height: ($whiteKey_Height * 0.55)
+$blackKey_Width: ($whiteKey_Width * 0.55)
+$blackKey_Offset: (- math.div($blackKey_Width, 1.8) - $border_Width)
+$fontSize: math.div($whiteKey_Width, 3)
+$whiteKey_Color: rgb(255,255,255)
+$blackKey_Color: rgb(0,0,0)
+$blackKey_BorderColor: rgb(0,0,0)
+$whiteKey_BorderColor: rgb(0,0,0)
+
+#keyboard
+  width: auto
+  height: 17vw
+  margin-top: 2vh
+  display: inline-block
+  margin-left: 2vw
+
+.key
+  float: left
+  position: relative
+  display: inline-block
+  cursor: pointer
+  text-align: center
+  font-size: $fontSize
+
+.white
+  padding-top: $whiteKey_Height
+  width: $whiteKey_Width
+  z-index: 1
+  border-top: $border_Width solid $whiteKey_BorderColor
+  border-bottom: $border_Width solid $whiteKey_BorderColor
+  border-left: $border_Width solid $whiteKey_BorderColor
+  background-color: $whiteKey_Color
+  color: black
+
+.black
+  padding-top: $blackKey_Height
+  width: $blackKey_Width
+  z-index: 2
+  border: $border_Width solid $blackKey_BorderColor
+  background-color: $blackKey_Color
+  color: white
+
+.black, .close
+  margin: 0 0 0 $blackKey_Offset
+
+.active
+  transform: scale(0.98)
+
+.black.active
+  background-color: lighten($blackKey_Color, 10%)
+
+.white.active
+  background-color: darken($whiteKey_Color, 10%)
+
+#E5
+  border-right: $border_Width solid $whiteKey_BorderColor
+
+@media screen and (max-width: 1145px)
+  #keyboard
+    width: auto
+    margin-left: 0
+    display: flex
+    justify-content: center
+</style>
